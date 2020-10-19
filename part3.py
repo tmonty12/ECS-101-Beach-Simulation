ab5b8f79337a75d3789ca21aacdf3bd7de380a55
tmonty12 <61255722+tmonty12@users.noreply.github.com>
2020-10-18 22:24:35 -0400

    Update part3.py
    The customer location can be a continous number between 0 and 10. The probability of a location is the score of the location divided by the total score. And the selection of a location needs to be random.

--- part3.py
+++ part3.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 def cafeshop(LA1, CA1, LA2, CA2):
     i = 0
     profitA1=0
@@ -10,33 +8,34 @@ def cafeshop(LA1, CA1, LA2, CA2):
     CB1=5
     LB2=8
     CB2=5
-    while i < 1000001:
-        buyer = random.randint(0, 10)
-        print(buyer)
+    trials = 1000000
+    while i <= trials:
+        customerLocation = random.random()*10
 
-        scoreA1 = (10 - abs(buyer - LA1)) + 3 * (6 - CA1)
-        scoreA2 = (10 - abs(buyer - LA2)) + 3 * (6 - CA2)
-        scoreB1 = (10 - abs(buyer - LB1)) + 3 * (6 - CB1)
-        scoreB2 = (10 - abs(buyer - LB2)) + 3 * (6 - CB2)
+        scoreA1 = (10 - abs(customerLocation - LA1)) + 3 * (6 - CA1)
+        scoreA2 = (10 - abs(customerLocation - LA2)) + 3 * (6 - CA2)
+        scoreB1 = (10 - abs(customerLocation - LB1)) + 3 * (6 - CB1)
+        scoreB2 = (10 - abs(customerLocation - LB2)) + 3 * (6 - CB2)
 
         totalScore=scoreA1+scoreA2+scoreB1+scoreB2
-        PA1 = scoreA1 + totalScore
-        PA2 = scoreA2 + totalScore
-        PB1 = scoreB1 + totalScore
-        PB2 = scoreB2 + totalScore
 
-        if PA1 > PB1 and PA2 > PB2:
+        PA1 = scoreA1 / totalScore
+        PA2 = scoreA2 / totalScore
+
+        randomChoice = random.random()
+
+        if PA1 >= randomChoice:
             profitA1 = profitA1 + (CA1 - 2)
-        elif PA2 > PB1 and PA2 > PB2:
+        elif PA1+PA2 >= randomChoice:
             profitA2 = profitA2 + (CA2 - 2)
         i=i+1
 
     #PPP=profit per person
-    AvgPPPA1=profitA1/i
-    AvgPPPA2=profitA2/i
+    AvgPPPA1=profitA1/trials
+    AvgPPPA2=profitA2/trials
 
-    print(AvgPPPA1)
-    print(AvgPPPA2)
+    print("Average profit per person of A1: " + str(AvgPPPA1))
+    print("Average profit per person of A2: " + str(AvgPPPA2))
 
 if __name__=="__main__":
 
