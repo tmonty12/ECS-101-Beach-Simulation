19214d62453d96dee08160d84fb25885f7c1f808
tmonty12 <61255722+tmonty12@users.noreply.github.com>
2020-10-22 15:08:27 -0400

    commenting the code

--- part3.py
+++ part3.py
@@ -2,48 +2,60 @@ import random
 import time
 
 def cafeshop(T, LA1, CA1, LA2, CA2):
-    i = 0
+    i = 0 
     profitA1=0
     profitA2=0
+    # Location and Costs for B1 and B2
     LB1=4
     CB1=5
     LB2=8
     CB2=5
 
+    # Loops through T trials
     while i <= T:
+        # Randomly generate the customer's location (0-10)
         customerLocation = random.random()*10
 
+        # Calculate the scores for A1, A2, B1, B2
         scoreA1 = (10 - abs(customerLocation - LA1)) + 3 * (6 - CA1)
         scoreA2 = (10 - abs(customerLocation - LA2)) + 3 * (6 - CA2)
         scoreB1 = (10 - abs(customerLocation - LB1)) + 3 * (6 - CB1)
         scoreB2 = (10 - abs(customerLocation - LB2)) + 3 * (6 - CB2)
 
+        # Calculate the total score to use for probability
         totalScore=scoreA1+scoreA2+scoreB1+scoreB2
 
+        # Calculate the probability of a customer choosing A1 and A2
         PA1 = scoreA1 / totalScore
         PA2 = scoreA2 / totalScore
 
+        # Randomly generate the customer's choice
         randomChoice = random.random()
 
+        # If the random choice falls in the range of PA1, add the profit to A1
         if PA1 >= randomChoice:
             profitA1 = profitA1 + (CA1 - 2)
+        # If the random choice falls in the range of PA@, add the profit to A2
         elif PA1+PA2 >= randomChoice:
             profitA2 = profitA2 + (CA2 - 2)
         i=i+1
 
-
+    # Program returns the total profit of A1 and A2 after T trials
     return [profitA1, profitA2]
 
 if __name__=="__main__":
     T = 1000000
 
+    # User inputs the cost and locations for A1 and A2
     LA1 = eval(input("Enter the location of A1: "))
     CA1 = eval(input("Enter the price of one cup of coffee at A1: "))
 
     LA2 = eval(input("Enter the location of A2: "))
     CA2 = eval(input("Enter the price of one cup of coffee at A2: "))
 
+    # Runs the program on the inputs
     profitA1, profitA2 = cafeshop(T,LA1,CA1,LA2,CA2)
 
+    # Prints the average profit per person for A1 and A1
     print("Average profit per person for A1: $" + str(round(profitA1/T,2)))
     print("Average profit per person for A2: $" + str(round(profitA2/T,2)))
--- part4.py
+++ part4.py
@@ -1,41 +1,53 @@
-import numpy as np
 import random
-import time
 from part3 import cafeshop
 
 def optimizer():
+    # Has the user input costs and locations for A1 and A2 (should not really matter in the end)
     la1 = eval(input("Enter the location of A1: "))
     ca1 = eval(input("Enter the price of one cup of coffee at A1: "))
     la2 = eval(input("Enter the location of A2: "))
     ca2 = eval(input("Enter the price of one cup of coffee at A2: "))
+    # Only run 100,000 trials - 1,000,000 would take too much time and not gain relevant accuracy
     trials = 100000
 
+    # Calculate the beginning profit using the program from part 3
     profitA1, profitA2 = cafeshop(trials, la1, ca1, la2, ca2)
     beginningProfit = profitA1 + profitA2
     print('\nYour beginning average profit per person for A1 and A2 is: $' + str(round(beginningProfit/trials,2)) +'\n')
 
+    # Store the beginning profit in totalProfit, this variable will be used to determine
+    # if a possible combination yields a greater profit
     totalProfit = beginningProfit
 
-    locationSteps = []
+    # Creates a list of locations to loop through and simulate
+    locationSteps = [] #[0,0.5,1,1.5,2 ...10]
     for i in range(0,21):
         locationSteps.append(i/2)
 
-    cashSteps = []
+    # Creates a list of costs to loop through and simulate
+    costSteps = [] # [2,2.25,2.5 ...6]
     for i in range(8,25):
-        cashSteps.append(i/4)
+        costSteps.append(i/4)
 
-    for x in cashSteps:
-        for y in cashSteps:
+    # Double for loop goes through every possible cost combination for A1 and A2 given the costSteps
+    # while holding the location variables fixed
+    for x in costSteps:
+        for y in costSteps:
+            # Calculate the profit of costs x = ca1 and y = ca2
             profitA1, profitA2 = cafeshop(trials, la1, x, la2, y)
             currentTotalProfit = profitA1 + profitA2
+            # Determine if the current cost combination yields a greater profit
             if(currentTotalProfit > totalProfit):
+                # If so the new max profit will be stored in totalProfit, and the new costs of A1 and A2 are stored
                 totalProfit = currentTotalProfit
                 ca1 = x
                 ca2 = y
                 print('C(A1): ' + str(x), 'C(A2): ' + str(y), 'Current TAPPP: $' + str(round(totalProfit/trials,2)))
 
+    # This print statement occurs after the cost has been optimized
     print('\nC(A1): ' + str(ca1), 'C(A2): ' + str(ca2), 'Current TAPPP: $' + str(round(totalProfit/trials,2)) + '\n')
 
+    # Same logic as with the previous double loop except now we are optimizing the locations of A1 and A2
     for i in locationSteps:
         for j in locationSteps:
             profitA1, profitA2 = cafeshop(trials, i, ca1, j, ca2)
@@ -47,6 +59,8 @@ def optimizer():
                 print('L(A1): ' + str(i), 'L(A2): ' + str(j), 'Current TAPPP: $' + str(round(totalProfit/trials,2)))
     print('\nL(A1): ' + str(la1), 'L(A2): ' + str(la2), 'Current TAPPP: $' + str(round(totalProfit/trials,2)) + '\n')
 
+    # These print statements output the best combinations of cost and location for A1 and A2, show the final profit per person
+    # and show the change in total profit per person from the beginning to the end
     print('L(A1): ' + str(la1), 'C(A1): $' + str(ca1), 'L(A2): ' + str(la2), 'C(A2): $' + str(ca2))
     print('Final Profit per person: $' + str(round(totalProfit/trials,2)))
     print('Change in total profit per person: $' + str(round((totalProfit-beginningProfit)/trials,2)) + '\n')
